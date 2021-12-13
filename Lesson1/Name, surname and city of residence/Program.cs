5fd42f9a000d3e824f5330d223a95563b838bf89
Renar <renar245@mail.ru>
2021-12-13 17:54:43 +0700

    add Name,surname and city of resident

---
+++ Lesson1/Name, surname and city of residence/Program.cs
@@ -0,0 +1,19 @@
+﻿// Автор: Кочетков Михаил 
+//Написать программу, которая выводит на экран ваше имя, фамилию и город проживания 
+
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string name = "Kochetkov Mikhail";
+		string city = "Krasnoyarsk";
+		
+		Console.SetCursorPosition(Console.WindowWidth / 3, Console.WindowHeight / 3);
+		Console.WriteLine(name);
+		Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+		Console.WriteLine(city);
+	}
+}
